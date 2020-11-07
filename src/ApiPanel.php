732a05b9ba362786c43ceca8518de6cfef68298b
Robert Mikšík <robertmiksik@gmail.com>
2020-11-07 16:28:46 +0100

    Panel

--- src/ApiPanel.php
+++ src/ApiPanel.php
@@ -4,14 +4,24 @@
 namespace Podnik;
 
 
+use Tracy\Debugger;
 use Tracy\IBarPanel;
 
 class ApiPanel implements IBarPanel
 {
     static $apis = [];
 
-    static function registerApi($api) {
-        self::$apis[] = $api;
+    static function startQuery($apiName, $method, $params = []) {
+        Debugger::timer($apiName."_".$method);
+    }
+
+    static function endQuery($apiName, $method, $params = []) {
+        $time = Debugger::timer($apiName."_".$method);
+        self::$apis[$apiName][] = [
+            "method" => $method,
+            "params" => $params,
+            "time" => round($time, 4)." s"
+        ];
     }
 
     /**
@@ -30,10 +40,15 @@ class ApiPanel implements IBarPanel
      */
     function getPanel()
     {
-        $output = "";
-        foreach (self::$apis as $api) {
-            $output .= "<li>$api</li>";
+        $output = "<h1>API Queries</h1><div class='tracy-inner tracy-InfoPanel'><div class='tracy-inner-container'><table class='tracy-sortable'><tbody>";
+        foreach (self::$apis as $api => $queries) {
+            $output .= "<strong>$api</strong>";
+            foreach ($queries as $query) {
+                $params = json_encode($query["params"]);
+                $output .= "<tr><td>$query[method]</td><td>$params</td><td>$query[time]</td></tr>";
+            }
         }
-        return "APIS: <br> $output";
+        $output .= "</tbody></div></div>";
+        return "$output";
     }
 }
